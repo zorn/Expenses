20f3e71632ea52334b49bc68e5243e029deb3fa6
Michael Zornek <mike@clickablebliss.com>
2015-01-04 20:54:00 -0500

    Adding comment about cell dequeue implementation.

--- _FINAL/Expenses/ExpenseListViewController.swift
+++ _FINAL/Expenses/ExpenseListViewController.swift
@@ -7,6 +7,7 @@ class ExpenseListViewController : UITableViewController, UITableViewDataSource, 
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // FIXME: What I don't like about this implementation is that I'm duplicating the "desgin" of the "Basic" cell in the storyboard and here in the else branch. I want to keep the storyboard version as it lets me make a segue. Preferably I'd have the app crash if the storyboard design was not available. I could do that by explicitly unpacking the optional I get back from dequeueReusableCellWithIdentifier but that feels "wrong" -- feedback welcome.
         let cellReuseID = "Basic"
         if let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseID) as? UITableViewCell {
             return self.configureCell(cell, indexPath: indexPath)
